a1ba2cdb9c5557a1626b563031814543c42a5103
Mariuz123 <andrea.mariutti07@gmail.com>
2025-12-09 09:48:08 +0100

    Modifiche

--- PHP/DocRoot/spesalick/connect.php
+++ PHP/DocRoot/spesalick/connect.php
@@ -2,12 +2,12 @@
     
     $host = 'localhost';
     $user = 'root';
+    $dbname = 'market';
     $password = '';
 
     try{
-        $conn = new PDO("mysql:host=$host;", $user, $password);
+        $conn = new PDO("mysql:host=$host; dbname=$dbname", $user, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connessione al database riuscita <br>";
 
         $name = $_POST['name'];
         $sname = $_POST['Surname'];
@@ -15,18 +15,27 @@
         $pwd = $_POST['pwd'];
         $product = $_POST['prduct'];
 
-        echo "Nome: $name, Surname: $sname, eMail: $eMail, pwd: $pwd, product: $product <br>";
-
-        $statement = $conn->prepare("INSERT INTO utenti(nome, sname, eMail, pwd, product) VALUES (:nome, :sname, :eMail, :pwd, :product)");
+        $statement = $conn->prepare("SELECT * FROM utenti WHERE nome = :nome AND sname = :sname AND eMail = :eMail");
         $statement->bindParam(':nome', $name);
         $statement->bindParam(':sname', $sname);
         $statement->bindParam(':eMail', $eMail);
-        $statement->bindParam(':pwd', $pwd);
-        $statement->bindParam(':product', $product);
-
         $statement->execute();
 
-echo "New line";
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        if(!count($result) > 0){
+            $statement = $conn->prepare("INSERT INTO utenti(nome, sname, eMail, pwd, product) VALUES (:nome, :sname, :eMail, :pwd, :product)");
+            $statement->bindParam(':nome', $name);
+            $statement->bindParam(':sname', $sname);
+            $statement->bindParam(':eMail', $eMail);
+            $statement->bindParam(':pwd', $pwd);
+            $statement->bindParam(':product', $product);
+
+            $statement->execute();
+        }   
+
+        include 'common/store.html';
+
     } catch (PDOException $e){
         echo $e -> getMessage();
     }
